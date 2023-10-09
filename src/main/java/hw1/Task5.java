7232784cd987113471ab30b8be8290e033377a7a
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-10 01:58:02 +0500

    hw1 Task5 done

---
+++ src/main/java/hw1/Task5.java
@@ -0,0 +1,41 @@
+package hw1;
+
+import java.util.stream.IntStream;
+
+public class Task5 {
+   public static boolean isPalindromeDescendant(int num) {
+       try {
+           String numStr = String.valueOf(num);
+
+           while (numStr.length() > 1) {
+               if (isPalindrome(numStr)) {
+                   return true;
+               }
+               numStr = getDescendant(numStr);
+           }
+           return false;
+       } catch (NullPointerException e) {
+           return false;
+       }
+   }
+   private static boolean isPalindrome(String numStr) {
+        int len = numStr.length();
+        int r = len / 2 - len % 2;
+        return IntStream
+            .range(0, r)
+            .allMatch(idx -> numStr.charAt(idx) == numStr.charAt(len - idx - 1));
+   }
+   private static String getDescendant(String numStr) {
+        int len = numStr.length();
+        StringBuilder result = new StringBuilder(len);
+
+       for (int i = 0; i < len - len % 2 - 1; i += 2) {
+           result.append(numStr.charAt(i) - '0' + numStr.charAt(i + 1) - '0');
+       }
+
+       result.append(
+           len % 2 == 1 ? numStr.charAt(len - 1) : ""
+       );
+       return result.toString();
+   }
+}
