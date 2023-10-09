0e37981d2461791a3d200216b4d2c050835c3be2
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-10 02:13:36 +0500

    hw1 Task5 Test done

---
+++ src/test/java/hw1/Task5Test.java
@@ -0,0 +1,35 @@
+package hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static hw1.Task5.isPalindromeDescendant;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task5Test {
+
+    @DisplayName("Check Task5 with no palindrome")
+    @ParameterizedTest(name = "num = {0}")
+    @ValueSource(ints = {
+        0,
+        -9,
+        50,
+        143,
+        5325
+    })
+    public void isNotPalindromeDescendantTest(int num) {
+        assertThat(isPalindromeDescendant(num)).isFalse();
+    }
+
+    @DisplayName("Check Task5 with palindrome")
+    @ParameterizedTest(name = "num = {0}")
+    @ValueSource(ints = {
+        11,
+        121,
+        65,
+        11211230
+    })
+    public void isPalindromeDescendantTest(int num) {
+        assertThat(isPalindromeDescendant(num)).isTrue();
+    }
+}
