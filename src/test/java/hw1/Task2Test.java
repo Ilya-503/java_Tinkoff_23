90714dafba199dbec8ee1f42fd69b368019822e3
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-09 20:05:31 +0500

    hw1 Task2 Test done

---
+++ src/test/java/hw1/Task2Test.java
@@ -0,0 +1,23 @@
+package hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static hw1.Task2.getNumLen;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task2Test {
+
+    @DisplayName("Check Task2 with legal data")
+    @ParameterizedTest(name = "num = {0}")
+    @CsvSource({
+        "-432, 3",
+        "0, 1",
+        "-0, 1",
+        "-4200, 4"
+    })
+    public void getNumLenWithLegalDataTest(int num, int expectedNumLen) {
+        int numLen = getNumLen(num);
+        assertThat(numLen).isEqualTo(expectedNumLen);
+    }
+}
