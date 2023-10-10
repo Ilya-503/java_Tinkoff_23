c8004e195547350060acffe3be6b33a579224074
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-10 15:44:45 +0500

    hw1 Task6 done

---
+++ src/main/java/hw1/Task6.java
@@ -0,0 +1,31 @@
+package hw1;
+
+public class Task6 {
+    private static final short KAPREKAR_CONST = 6174;
+    public static int countK(int num) {
+        if (num < 1001 || num > 9999 || hasAllSameDig(num)) {
+            return -1;
+        }
+
+        int minNum = 0, maxNum = 0;
+        int[] digChars = String.valueOf(num).chars().sorted().toArray();
+
+        for (byte i = 0; i < 4; i++) {
+            int digit = digChars[i] - '0';
+            minNum = minNum * 10 + digit;
+            maxNum = maxNum + digit * (int) Math.pow(10, i);
+        }
+
+        int diff = maxNum - minNum;
+        if (diff == KAPREKAR_CONST) {
+            return 1;
+        }
+        return 1 + countK(diff);
+    }
+    private static boolean hasAllSameDig(int num) {
+        return String
+            .valueOf(num)
+            .chars()
+            .allMatch(el -> el - '0' == num % 10);
+    }
+}
