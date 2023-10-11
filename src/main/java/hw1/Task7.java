ed8bcc5ba7ec878234203447b5f53017be7ff044
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-11 10:26:18 +0500

    fixed rotateRight Task7

--- src/main/java/hw1/Task7.java
+++ src/main/java/hw1/Task7.java
@@ -1,11 +1,39 @@
 package hw1;
 
+import javassist.bytecode.ByteArray;
+import java.math.BigInteger;
+import java.util.BitSet;
+
 public class Task7 {
 
-    public static int rotateLeft(int num, int bitAm) {
-        return (num << bitAm) | (num >> (Integer.SIZE - bitAm % Integer.SIZE));
+    public static void main(String[] args) {
+        System.out.println(rotateRight(9, 3));
     }
+
     public static int rotateRight(int num, int bitAm) {
-        return (num >> bitAm) | (num << (Integer.SIZE - bitAm % Integer.SIZE));
+        BitSet inputBits = BitSet.valueOf(
+            BigInteger
+                .valueOf(num)
+                .toByteArray()
+        );
+        int len = inputBits.length();
+        BitSet resBits = new BitSet(len);
+        int finalBitAm = bitAm % len;       // for use in lambda
+
+        inputBits.stream().forEach(
+            bit -> {
+                int newBit = bit - finalBitAm;
+                resBits.set(
+                    newBit < 0 ? len + newBit : newBit
+                );
+            }
+        );
+        return resBits
+            .stream()
+            .reduce(0,
+                (res, bit) -> res + (int) Math.pow(2, bit)
+            );
     }
+
+
 }
