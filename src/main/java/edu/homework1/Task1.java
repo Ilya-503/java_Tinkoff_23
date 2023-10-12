0eee227c90a919bccbe481c2d550bb6833d9fde2
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-12 16:46:42 +0500

    fixed Task1 Runtime exceptions catch

--- src/main/java/edu/homework1/Task1.java
+++ src/main/java/edu/homework1/Task1.java
@@ -1,32 +1,29 @@
 package edu.homework1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.regex.Pattern;
 
 public final class Task1 {
 
     private Task1() {
     }
 
-    private static final Logger LOGGER = LogManager.getLogger();
     private static final int SECS_IN_MIN = 60;
+    private static final int ERR_RETURN = -1;
+    private static final Pattern LEGAL_STR_LEN_PATTERN = Pattern.compile("^[\\d]+:[\\d]{2}$");
 
     public static int getVideoLenInSec(final String lenStr) {
-        try {
-            String[] lenArr = lenStr.split(":");
-            int minutes = Integer.parseInt(lenArr[0]);
-            int seconds = Integer.parseInt(lenArr[1]);
-
-            if (seconds < 0 || seconds + 1 > SECS_IN_MIN || minutes < 0) {
-                throw new IllegalArgumentException();
-            }
-            return minutes * SECS_IN_MIN + seconds;
-
-        } catch (IllegalArgumentException
-                 | ArrayIndexOutOfBoundsException
-                 | NullPointerException e) {
-            LOGGER.error("Неверный формат длины видео ");
-            return -1;
+        if (lenStr == null
+            || !LEGAL_STR_LEN_PATTERN.matcher(lenStr).matches()) {
+            return ERR_RETURN;
         }
+        String[] lenArr = lenStr.split(":");
+        int minutes = Integer.parseInt(lenArr[0]);
+        int seconds = Integer.parseInt(lenArr[1]);
+
+        if (seconds < 0 || seconds + 1 > SECS_IN_MIN || minutes < 0) {
+            return ERR_RETURN;
+        }
+        return minutes * SECS_IN_MIN + seconds;
     }
 }
+
