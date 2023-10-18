031f2b0e7a6a8cd9d1efb850641fedda76cf8320
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-18 15:00:15 +0500

    hw2 Task1 fix: removed double inputs

--- src/main/java/edu/hw2/Task1/Expression.java
+++ src/main/java/edu/hw2/Task1/Expression.java
@@ -11,6 +11,11 @@ public sealed interface Expression {
         public double evaluate() {
             return constant;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(constant);
+        }
     }
 
     final class Negate implements Expression {
@@ -21,13 +26,14 @@ public sealed interface Expression {
             negConstant = -expr.evaluate();
         }
 
-        public Negate(double constant) {
-            negConstant = -constant;
-        }
-
         @Override
         public double evaluate() {
             return negConstant;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(negConstant);
         }
     }
 
@@ -35,43 +41,24 @@ public sealed interface Expression {
 
         private final double expResult;
 
-        public Exponent(double foot, double degree) {
-            expResult = pow(foot, degree);
-        }
-
-        public Exponent(Expression foot, double degree) {
+        public Exponent(Expression foot, int degree) {
             expResult = pow(foot.evaluate(), degree);
-        }
-
-        public Exponent(double foot, Expression degree) {
-            expResult = pow(foot, degree.evaluate());
-        }
-
-        public Exponent(Expression foot, Expression degree) {
-            expResult = pow(foot.evaluate(), degree.evaluate());
         }
 
         @Override
         public double evaluate() {
             return expResult;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(expResult);
+        }
     }
 
     final class Addition implements Expression {
 
         private final double sumResult;
-
-        public Addition(double a, double b) {
-            sumResult = a + b;
-        }
-
-        public Addition(Expression a, double b) {
-            sumResult = a.evaluate() + b;
-        }
-
-        public Addition(double a, Expression b) {
-            sumResult = a + b.evaluate();
-        }
 
         public Addition(Expression a, Expression b) {
             sumResult = a.evaluate() + b.evaluate();
@@ -81,23 +68,16 @@ public sealed interface Expression {
         public double evaluate() {
             return sumResult;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(sumResult);
+        }
     }
 
-    public final class Multiplication implements Expression {
+    final class Multiplication implements Expression {
 
         private final double multResult;
-
-        public Multiplication(double a, double b) {
-            multResult = a * b;
-        }
-
-        public Multiplication(Expression a, double b) {
-            multResult = a.evaluate() * b;
-        }
-
-        public Multiplication(double a, Expression b) {
-            multResult = a * b.evaluate();
-        }
 
         public Multiplication(Expression a, Expression b) {
             multResult = a.evaluate() * b.evaluate();
@@ -106,6 +86,11 @@ public sealed interface Expression {
         @Override
         public double evaluate() {
             return multResult;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(multResult);
         }
     }
 }
