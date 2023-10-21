2a2442e18201a6aadcc8c1e083db8893a8b48b1a
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-21 18:38:44 +0500

    added game console

--- src/main/java/edu/project1/GameConsole.java
+++ src/main/java/edu/project1/GameConsole.java
@@ -1,8 +1,19 @@
 package edu.project1;
 
+import java.lang.reflect.Parameter;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import static edu.project1.InputType.GIVE_UP;
+import static edu.project1.InputType.LETTER;
+import static edu.project1.Utils.isMatchingRegex;
 
 public final class GameConsole {
+
+    private static final String COMMAND_REG_TEMPLATE = "^[\\s]*%s[\\s]*$";
+    private static final String GIVE_UP_COMMAND ="gp";
+    private static final String LETTER_REGEX = String.format(COMMAND_REG_TEMPLATE, "[a-zA-z]");
+    private static final String GIVE_UP_REGEX = String.format(COMMAND_REG_TEMPLATE, GIVE_UP_COMMAND);
 
     private final Scanner reader;
 
@@ -10,8 +21,37 @@ public final class GameConsole {
         reader = new Scanner(System.in);
     }
 
-    String readLetter() {
-        return reader.next();
+    public String getGiveUpCommand() {
+        return GIVE_UP_COMMAND;
+    }
+
+    InputType getUserInput() {
+
+        boolean isLetter = false;
+        boolean isGivingUp = false;
+
+        do {
+            String userInput = readString();
+            isLetter = isMatchingRegex(userInput, LETTER_REGEX);
+            isGivingUp = isMatchingRegex(userInput, GIVE_UP_REGEX);
+
+        } while (!isGivingUp && !isLetter);
+
+        if (isLetter) {
+            return LETTER;
+        }
+        return GIVE_UP;
+    }
+
+
+
+    private String readString() {
+        String str;
+        printMsg("Try guess: ");
+        str = reader.nextLine();
+        printMsg("");
+
+        return str;
     }
 
     void printMsg(String msg) {
