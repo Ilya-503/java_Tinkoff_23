4c9dd5ec69e57e602c1e0e270b352f6b1fa3fe8d
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-10-23 01:56:52 +0500

    checkstyle bug

--- src/main/java/edu/project1/Game/UpdatingFinishedGameException.java
+++ src/main/java/edu/project1/Game/UpdatingFinishedGameException.java
@@ -2,15 +2,7 @@ package edu.project1.Game;
 
 public final class UpdatingFinishedGameException extends RuntimeException {
 
-    UpdatingFinishedGameException() {
-        super();
-    }
-
     UpdatingFinishedGameException(String msg) {
         super(msg);
-    }
-
-    UpdatingFinishedGameException(String msg, Throwable cause) {
-        super(msg, cause);
     }
 }
