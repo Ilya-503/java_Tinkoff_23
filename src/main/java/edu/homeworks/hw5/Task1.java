318b913eb74004535e285bbc2ee533690428043c
Ilya-503 <90442003+Ilya-503@users.noreply.github.com>
2023-11-07 05:49:54 +0500

    Task1 regex little fix

--- src/main/java/edu/homeworks/hw5/Task1.java
+++ src/main/java/edu/homeworks/hw5/Task1.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public final class Task1 {
     private static final Pattern LEGAL_DATE_STRING_REGEX =
-        Pattern.compile("(\\d{4}-\\d{2}-\\d{2}), (\\d{2}:\\d{2}) - (\\d{4}-\\d{2}-\\d{2}), (\\d{2}:\\d{2})");
+        Pattern.compile("^(\\d{4}-\\d{2}-\\d{2}), (\\d{2}:\\d{2}) - (\\d{4}-\\d{2}-\\d{2}), (\\d{2}:\\d{2})$");
 
     private Task1() {
     }
